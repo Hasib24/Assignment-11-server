079f5a990d5f74212900bc6fc1152d9336aaef8e
Hasib24 <hasibislam1488@gmail.com>
2023-05-19 15:16:27 +0600

    add delete api

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
 require('dotenv').config()
 const app = express()
@@ -45,6 +45,18 @@ async function run() {
       const result = await toyCollection.insertOne(data)
    
       res.send(result)
+    })
+
+    app.get('/mytoys', async(req, res)=>{
+      const result = await toyCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.delete('/mytoys/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)};
+      const result = await toyCollection.deleteOne(query)
+      res.send(result);
     })
     
 
