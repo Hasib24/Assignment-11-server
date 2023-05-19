8b6fd313326f895ad976b7d9295cfe703065ac0a
Hasib24 <hasibislam1488@gmail.com>
2023-05-20 00:30:56 +0600

    add api for specific id

--- index.js
+++ index.js
@@ -60,6 +60,13 @@ async function run() {
       const result = await toyCollection.deleteOne(query)
       res.send(result);
     })
+
+    app.get('/update/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)};
+      const result = await toyCollection.find(query).toArray()
+      res.send(result);
+    })
     
 
     // Send a ping to confirm a successful connection
